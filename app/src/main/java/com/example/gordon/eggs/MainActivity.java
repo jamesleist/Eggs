7b968cd25d6d50e5fd5566e96eb1467e420b8951
vgwiss <victor.wiss.15@cnu.edu>
2017-04-20 17:49:59 -0400

    Button Methods Added

--- app/src/main/java/com/example/gordon/eggs/MainActivity.java
+++ app/src/main/java/com/example/gordon/eggs/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.gordon.eggs;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addOneEgg(View view) {
+
+    }
+
+    public void addTwoEgg(View view) {
+
+    }
+
+    public void subtractOneEgg(View view) {
+
+    }
+
+    public void makeBreakfast(View view) {
+
     }
 }
