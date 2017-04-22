55100c06f2d9ecf69a179aa710ecfb145c7fc39b
James F. Leist <james.leist.13@cnu.edu>
2017-04-22 17:26:38 -0400

    Broadcast Receiver framework set up.

--- app/src/main/java/com/example/gordon/eggs/MainActivity.java
+++ app/src/main/java/com/example/gordon/eggs/MainActivity.java
@@ -1,31 +1,39 @@
 package com.example.gordon.eggs;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Intent myIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void addOneEgg(View view) {
-
+        makeEggIntent("1");
     }
 
     public void addTwoEgg(View view) {
-
+        makeEggIntent("2");
     }
 
     public void subtractOneEgg(View view) {
-
+        makeEggIntent("-1");
     }
 
     public void makeBreakfast(View view) {
-
+        myIntent = new Intent("com.example.gordon.eggs.MAKE_BREAKFAST");
+        sendBroadcast(myIntent);
+    }
+    private void makeEggIntent(String number_of_eggs){
+        myIntent = new Intent("com.example.gordon.eggs.ADD_EGGS");
+        myIntent.putExtra("number_of_eggs", number_of_eggs);
+        sendBroadcast(myIntent);
     }
 }
---
+++ app/src/main/java/com/example/gordon/eggs/MyBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.example.gordon.eggs;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by james on 4/22/2017.
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
