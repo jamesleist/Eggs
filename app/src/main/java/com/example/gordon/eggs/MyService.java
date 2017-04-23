24dd7864d65294b58226360d0c79daed8b5365e6
James F. Leist <james.leist.13@cnu.edu>
2017-04-23 13:45:25 -0400

    Only notifications left

--- app/src/main/java/com/example/gordon/eggs/MyService.java
+++ app/src/main/java/com/example/gordon/eggs/MyService.java
@@ -3,10 +3,13 @@ package com.example.gordon.eggs;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
 public class MyService extends Service {
+    private SharedPreferences.Editor editor;
+    private int eggNumber;
 
     public int onStartCommand(Intent intent, int flags, int startId) {
 
@@ -14,48 +17,64 @@ public class MyService extends Service {
         SharedPreferences prefs = getApplicationContext().
                 getSharedPreferences("com.example.gordon.eggs", MODE_PRIVATE);
 
-        SharedPreferences.Editor editor = prefs.edit();
+        editor = prefs.edit();
 
         //Ensures that the stored egg count is initialized to zero
-        if(prefs.getInt("eggs", 0) == 0) {
-            editor.putInt("eggs", 0);
-            editor.commit();
-        }
+        //if(prefs.getInt("eggs", 0) == 0) {
+        //    editor.putInt("eggs", 0);
+        //    editor.commit();
+        //}
 
         //Working egg amount
-        int eggNumber = prefs.getInt("eggs", 0);
-
-        //If the user wants to add eggs
-        if(intent.getBundleExtra("EggMode").equals("add_eggs")) {
-            int addingEggs = intent.getIntExtra("eggNumber", 0);
-            //Make sure that the working amount of eggs never dips below sero
-            if(eggNumber + addingEggs < 0) {
-                editor.putInt("eggs", 0);
-            }
-
-            //Adds eggs
-            else {
-                editor.putInt("eggs", eggNumber + addingEggs);
-            }
-
-            //Stores Egg amount changes to the SharedPreferences
-            editor.commit();
-        }
-
-        //If user wants to make breakfast
-        if(intent.getBundleExtra("EggMode").equals("make_breakfast")) {
-            //Make sure the working egg amount is at least six
-
-            if(eggNumber >= 6) {
-                editor.putInt("eggs", eggNumber - 6);
-                editor.commit();
+        eggNumber = prefs.getInt("eggs", 0);
 
 
-            }
-
+        //Check egg value
+        Bundle myBundle = intent.getExtras();
+        switch(myBundle.getInt("Eggs")){
+            case Constants.ADD_ONE_EGG:
+                eggNumber = Constants.ADD_ONE_EGG + eggNumber;
+                updateSharedPreference();
+                addEggsNotification();
+                break;
+            case Constants.ADD_TWO_EGGS:
+                eggNumber = Constants.ADD_TWO_EGGS + eggNumber;
+                updateSharedPreference();
+                addEggsNotification();
+                break;
+            case Constants.SUB_ONE_EGG:
+                eggNumber = (eggNumber == Constants.NULL_VALUE) ? eggNumber : Constants.SUB_ONE_EGG + eggNumber;
+                updateSharedPreference();
+                break;
+            case Constants.MAKE_BREAKFAST:
+                if(eggNumber < Constants.NUM_EGGS_OMELET){
+                    eggNumber = 0;
+                    updateSharedPreference();
+                    makeBreakfastNotification(false);
+                } else {
+                    eggNumber = eggNumber - Constants.NUM_EGGS_OMELET;
+                    updateSharedPreference();
+                    makeBreakfastNotification(true);
+                }
+                break;
+            default:
+                break;
         }
 
         return START_NOT_STICKY;
+    }
+
+    private void updateSharedPreference(){
+        editor.putInt("eggs", eggNumber);
+        editor.apply();
+    }
+
+    private void addEggsNotification(){
+
+    }
+
+    private void makeBreakfastNotification(boolean enough_for_omelet){
+
     }
 
     @Nullable
