bce707eb2c392782b27577373259196fa6d643b6
James F. Leist <james.leist.13@cnu.edu>
2017-04-23 17:04:08 -0400

    Potentially done?

--- app/src/main/java/com/example/gordon/eggs/Constants.java
+++ app/src/main/java/com/example/gordon/eggs/Constants.java
@@ -11,4 +11,10 @@ public class Constants {
     public static final int MAKE_BREAKFAST = 69;
     public static final int NUM_EGGS_OMELET = 6;
     public static final int NULL_VALUE = 0;
+    public static final String ADD_ONE = "1 added!";
+    public static final String ADD_TWO = "2 added!";
+    public static final String SUB_ONE = "1 subtracted!";
+    public static final String BREAKFAST_OMELET = "We are having omelets, we have ";
+    public static final String BREAKFAST_GRUEL = "We are having omelets, we have ";
+    public static final String EGGS_AVAILABLE = " eggs available";
 }
